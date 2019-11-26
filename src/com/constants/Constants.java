0ba759cdf0f40bc5a24b984b5e866815a1eb75b8
vlads99650@gmail.com <vlads99650@gmail.com>
2019-11-26 19:51:45 +0200

    Constants

---
+++ src/com/constants/Constants.java
@@ -0,0 +1,23 @@
+package com.constants;
+
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final int ZERO = 0;
+    public static final int KNIGHT_HP = 900;
+    public static final int WIZARD_HP = 400;
+    public static final int PYROMANCER_HP = 500;
+    public static final int ROGUE_HP = 600;
+
+    public static final int TEN = 10;
+    public static final int ITEMS = 15;
+    public static final int COINS = 80;
+    public static final int NINE = 9;
+    public static final int FIVE = 5;
+    public static final int ILLEGAL = 19;
+    public static final int SIZE = 21;
+    public static final int MINCOINS = 16;
+    public static final int THREE = 3;
+    public static final int EIGHT = 8;
+}
